e7029dda3b79443bfe15dc26f04fa523ab39da18
Anushri <sastikaranushri@gmail.com>
2026-01-19 21:03:59 -0500

    for loop approach

--- array/buy_and_sell_stock_II.py
+++ array/buy_and_sell_stock_II.py
@@ -5,36 +5,18 @@
 # But you must sell before you buy again
 # Maximize total profit from multiple buy–sell pairs
 
-
-# while loop based
+# for loop based
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        l = 0
+        r = 1
         total_profit = 0
-        l = 0  # buy pointer
-        r = 1  # sell pointer
 
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                total_profit += prices[r] - prices[l]
-            # move buy pointer to next day
-            l = r  
-            # move sell pointer to next day
-            r += 1  
+        for i in range(len(prices) - 1):
+            if prices[i] < prices[i+1]:
+                total_profit += prices[i+1] - prices[i]
 
         return total_profit
-    
-# for loop based
-# class Solution:
-#     def maxProfit(self, prices: list[int]) -> int:
-#         l = 0
-#         r = 1
-#         total_profit = 0
-
-#         for i in range(len(prices) - 1):
-#             if prices[i] < prices[i+1]:
-#                 total_profit += prices[i+1] - prices[i]
-
-#         return total_profit
 
 solution = Solution()
 print(solution.maxProfit([7,1,5,3,6,4]))
