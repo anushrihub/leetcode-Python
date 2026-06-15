7d4d5f9a9a7bcbcc2e9d337165c33924b4c62521
Anushri <sastikaranushri@gmail.com>
2026-06-15 18:23:38 -0400

    complexity

--- arrays_and_hashing/concatenation_of_array.py
+++ arrays_and_hashing/concatenation_of_array.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/concatenation-of-array/description/
+# time complexity- O(n) space complexity- O(n)
+
 class Solution:
     def getConcatenation(self, nums: list[int]):
 
